b26e36dd0515fd21780f98596bdb24c531fe71bd
k1rakishou <kez1911@protonmail.com>
2018-10-03 20:07:51 +0300

    Finally figured out how to make textArea scroll to bottom when inserting a new message (it's kinda hacky but whatever)

--- chat-testclient/src/main/java/ui/chat_main_window/ChatRoomView.kt
+++ chat-testclient/src/main/java/ui/chat_main_window/ChatRoomView.kt
@@ -2,11 +2,14 @@ package ui.chat_main_window
 
 import Styles
 import javafx.beans.property.SimpleStringProperty
+import javafx.scene.control.TextArea
 import javafx.scene.layout.Priority
 import tornadofx.*
 
 
 class ChatRoomView : View() {
+  private lateinit var textArea: TextArea
+
   private val model = object : ViewModel() {
     val chatMessages = bind { SimpleStringProperty() }
   }
@@ -15,15 +18,23 @@ class ChatRoomView : View() {
     textarea(model.chatMessages) {
       addClass(Styles.chatRoomTextArea)
       vboxConstraints { vGrow = Priority.ALWAYS }
+
+      isEditable = false
     }
     textfield {
       addClass(Styles.chatRoomTextField)
       promptText = "Enter your message here"
 
       setOnAction {
-        model.chatMessages.value = text
+        textArea.appendText("$text\n")
+
         clear()
       }
     }
+
+    //this is probably a fucking hack, but I have no idea
+    //how to access textArea from textField otherwise ¯\_(ツ)_/¯
+    textArea = getChildList()
+      ?.first { it is TextArea } as TextArea
   }
 }
