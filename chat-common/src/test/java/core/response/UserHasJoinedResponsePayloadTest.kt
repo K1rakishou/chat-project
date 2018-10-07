64039a3623c3b596e972b5dc52de4697f82676d2
k1rakishou <kez1911@protonmail.com>
2018-10-07 13:27:11 +0300

    Add more test to UserHasJoinedResponsePayloadTest

--- chat-common/src/test/java/core/response/UserHasJoinedResponsePayloadTest.kt
+++ chat-common/src/test/java/core/response/UserHasJoinedResponsePayloadTest.kt
@@ -1,7 +1,10 @@
 package core.response
 
+import core.Constants
 import core.Status
+import core.exception.ByteSinkReadException
 import core.model.drainable.PublicUserInChat
+import core.security.SecurityUtils
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -32,6 +35,40 @@ class UserHasJoinedResponsePayloadTest : BaseResponsePayloadTest() {
       assertEquals(status, restoredResponse.status)
       assertEquals(null, restoredResponse.roomName)
       assertEquals(null, restoredResponse.user)
+    })
+  }
+
+  @Test(expected = ByteSinkReadException::class)
+  fun testResponseExceedUserNameSize() {
+    val roomName = "test room"
+    val userName = SecurityUtils.Generation.generateRandomString(Constants.maxUserNameLen + 10)
+
+    val user = PublicUserInChat(userName, ByteArray(266) { 0xF0.toByte() })
+
+    testPayload(UserHasJoinedResponsePayload.success(roomName, user), { byteSink ->
+      UserHasJoinedResponsePayload.fromByteSink(byteSink)
+    }, { restoredResponse ->
+      assertEquals(Status.Ok, restoredResponse.status)
+      assertEquals(roomName, restoredResponse.roomName)
+      assertEquals(user.userName, restoredResponse.user!!.userName)
+      assertArrayEquals(user.ecPublicKey, restoredResponse.user!!.ecPublicKey)
+    })
+  }
+
+  @Test(expected = ByteSinkReadException::class)
+  fun testResponseExceedEcPublicKeySize() {
+    val roomName = "test room"
+    val ecPublicKey = SecurityUtils.Generation.generateRandomString(Constants.maxEcPublicKeySize + 10).toByteArray()
+
+    val user = PublicUserInChat("test user", ecPublicKey)
+
+    testPayload(UserHasJoinedResponsePayload.success(roomName, user), { byteSink ->
+      UserHasJoinedResponsePayload.fromByteSink(byteSink)
+    }, { restoredResponse ->
+      assertEquals(Status.Ok, restoredResponse.status)
+      assertEquals(roomName, restoredResponse.roomName)
+      assertEquals(user.userName, restoredResponse.user!!.userName)
+      assertArrayEquals(user.ecPublicKey, restoredResponse.user!!.ecPublicKey)
     })
   }
 }
