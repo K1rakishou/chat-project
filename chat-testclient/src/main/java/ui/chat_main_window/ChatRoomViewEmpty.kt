35ff5f2e4930d7b258e66bb5f8b05975cd429545
k1rakishou <kez1911@protonmail.com>
2018-10-14 17:22:40 +0300

    Fix ChatRoomViewEmpty message not being in the center of the view

--- chat-testclient/src/main/java/ui/chat_main_window/ChatRoomViewEmpty.kt
+++ chat-testclient/src/main/java/ui/chat_main_window/ChatRoomViewEmpty.kt
@@ -1,14 +1,16 @@
 package ui.chat_main_window
 
 import javafx.geometry.Pos
+import javafx.scene.layout.Priority
 import tornadofx.*
 
 class ChatRoomViewEmpty : View() {
 
   override val root = vbox {
+    vboxConstraints { vGrow = Priority.ALWAYS }
     alignment = Pos.CENTER
 
-    label("Select chat room to display it") {
+    label("Select chat room to join") {
       addClass(Styles.chatRoomViewEmptyLabel)
     }
   }
