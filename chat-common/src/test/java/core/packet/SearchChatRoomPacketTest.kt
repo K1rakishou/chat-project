9b08ee6a817711806fd83240dc114a3d87a85275
k1rakishou <kez1911@protonmail.com>
2018-11-05 21:04:55 +0300

    Add tests for SearchChatRoomPacket

---
+++ chat-common/src/test/java/core/packet/SearchChatRoomPacketTest.kt
@@ -0,0 +1,43 @@
+package core.packet
+
+import core.Constants
+import core.exception.PacketDeserializationException
+import core.security.SecurityUtils
+import org.junit.Assert.*
+import org.junit.Test
+
+class SearchChatRoomPacketTest : BasePacketPayloadTest() {
+
+  @Test
+  fun `test packet`() {
+    val chatRoomName = "34234"
+
+    testPayload(SearchChatRoomPacket(chatRoomName), { byteSink ->
+      SearchChatRoomPacket.fromByteSink(byteSink)
+    }, { restoredPacket ->
+      assertEquals(chatRoomName, restoredPacket.chatRoomName)
+    })
+  }
+
+  @Test
+  fun `test packet with empty chatRoomName`() {
+    val chatRoomName = ""
+
+    testPayload(SearchChatRoomPacket(chatRoomName), { byteSink ->
+      SearchChatRoomPacket.fromByteSink(byteSink)
+    }, { restoredPacket ->
+      assertEquals(chatRoomName, restoredPacket.chatRoomName)
+    })
+  }
+
+  @Test(expected = PacketDeserializationException::class)
+  fun `test packet with chatRoomName exceeding maxChatRoomNameLen`() {
+    val chatRoomName = SecurityUtils.Generator.generateRandomString(Constants.maxChatRoomNameLength + 10)
+
+    testPayload(SearchChatRoomPacket(chatRoomName), { byteSink ->
+      SearchChatRoomPacket.fromByteSink(byteSink)
+    }, { restoredPacket ->
+      assertEquals(chatRoomName, restoredPacket.chatRoomName)
+    })
+  }
+}
